7254627b1c0de37797436871939e6fc91834cc67
Abdelhamed Elbadawy <abdelhamedelbadawy47@gmail.com>
2026-01-11 12:35:59 +0200

    Refactor role binding script to use dynamic role name

--- OCPcustomRoleBindingAutomated.sh
+++ OCPcustomRoleBindingAutomated.sh
@@ -1,9 +1,11 @@
-CUSTOM_ROLE_NAME=CUSTOM_EDIT
-CUSTOM_ROLE_BINDING_NAME=$1
+CUSTOM_ROLE_NAME=$1
 CUSTO_ROLE_NAMESPACE=IDENTITY_NAMESPACE
 for NAME_SPACE in $(oc get namespaces -o jsonpath='{.items[*].metadata.name}' | tr ' ' '\n')
 do 
     for GROUP in $(oc get groups | awk '{print $2}');do
-        oc adm policy add-role-to-group CUSTOM_ROLE_NAME  $GROUP --role-namespace=CUSTOM_ROLE_NAMESPACE --rolebinding-name=CUSTOM_ROLE_BINDING_NAME
+
+        echo " apllying the rolebinding '${CUSTOM_ROLE_NAME}_${NAME_SPACE}' for $GROUP on namespace $NAME_SPACE " |  \
+         oc adm policy add-role-to-group CUSTOM_ROLE_NAME \
+         $GROUP --role-namespace=CUSTOM_ROLE_NAMESPACE --rolebinding-name=${CUSTOM_ROLE_NAME}_${NAME_SPACE}
     done
 done
